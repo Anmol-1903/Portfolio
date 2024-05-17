db059d7283db802bc3b5f6cc57130bb214f68262
Anmol-1903 <anmolmittal1903@gmail.com>
2024-05-17 23:40:46 +0530

    Better Scroll Bar

--- resume/js/resumeHeight.js
+++ resume/js/resumeHeight.js
@@ -138,3 +138,11 @@ function smoothScrollNavBar(target) {
     }
 }
 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
