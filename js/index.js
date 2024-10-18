01fdd79d2119fe3093f830647ec7458c595c4316
Anmol Mittal <anmolmittal1903@gmail.com>
2024-10-18 13:21:13 +0530

    updated CV

--- js/index.js
+++ js/index.js
@@ -190,3 +190,15 @@ $(function(){
   $('.navigation-close').hover(cursorhover,cursor);
 
 })
+
+function clickIE(){if (document.all){return false;}}
+
+function clickNS(){if(document.layers ||(document.getElementById&&!document.all )){
+  if(e.which==2 || e.which==3){return false;}
+} }
+
+if(document.layers){
+  document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+  else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+  document.oncontextmenu=new Function("return false")
